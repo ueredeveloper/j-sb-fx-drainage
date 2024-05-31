9e70c042ef9186ce0e0f621c098581d8bc5dfe79
Fabrício José Barrozo <ueredeveloper@gmail.com>
2024-05-31 12:46:35 -0300

    inicia a mudança do mapa para leaflet

--- src/main/java/controllers/MainController.java
+++ src/main/java/controllers/MainController.java
@@ -6,16 +6,22 @@ import java.util.ResourceBundle;
 
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.concurrent.Worker;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
+import netscape.javascript.JSException;
+import netscape.javascript.JSObject;
+import com.sun.javafx.webkit.WebConsoleListener;
 
+@SuppressWarnings("restriction")
 public class MainController implements Initializable {
-	
 
 	@FXML
 	private AnchorPane apMain;
@@ -47,11 +53,38 @@ public class MainController implements Initializable {
 		return this.apMap;
 	}
 
+	@FXML
+	private Button btnZoom;
+
+	private JSObject doc;
+	private WebEngine webEngine;
+	public boolean ready;
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 
-		WebEngine webEngine = wvMap.getEngine();
+		webEngine = wvMap.getEngine();
 		webEngine.load(getClass().getResource("/html/map/index.html").toExternalForm());
+
+		webEngine.getLoadWorker().stateProperty().addListener(new ChangeListener<Worker.State>() {
+			public void changed(final ObservableValue<? extends Worker.State> observableValue,
+					final Worker.State oldState, final Worker.State newState) {
+				if (newState == Worker.State.SUCCEEDED) {
+					doc = (JSObject) webEngine.executeScript("window");
+
+					doc.setMember("app", MainController.this);
+
+					// doc.setMember("appShapeEndereco", GoogleMap.this);
+				}
+
+				// System.out.println(" initComunicantion funcionando");
+			}
+		});
+		
+		// mostrar erros no console
+        WebConsoleListener.setDefaultListener((webView, message, lineNumber, sourceId) -> {
+            System.out.println(message + "[at " + lineNumber + "]");
+        });
 
 		// Add a listener to the width property of the AnchorPane
 		apContent.widthProperty().addListener(new ChangeListener<Number>() {
@@ -75,8 +108,45 @@ public class MainController implements Initializable {
 
 		loadNavigationBar();
 
+		btnZoom.setOnAction(this::handleZoomButtonClick);
+
 	}
-	
+
+	private void invokeJS(final String str) {
+
+		if (ready) {
+			try {
+				doc.eval(str);
+			} catch (JSException js) {
+				System.out.println("nao ready execao de leitura javascript " + js);
+			}
+		} else {
+
+			webEngine.getLoadWorker().stateProperty().addListener(new ChangeListener<Worker.State>() {
+				@Override
+				public void changed(final ObservableValue<? extends Worker.State> observableValue,
+						final Worker.State oldState, final Worker.State newState) {
+					if (newState == Worker.State.SUCCEEDED) {
+						doc.eval(str);
+
+					}
+
+					// System.out.println(" invokeJS funcionando");
+				}
+			});
+
+		}
+	}
+
+	private void handleZoomButtonClick(ActionEvent event) {
+		// Call setZoom method
+		setZoom();
+	}
+
+	public void setZoom() {
+		invokeJS("setZoom();");
+	}
+
 	private DocumentController docController;
 	private AnchorPane apDocument; // Store the AnchorPane of Documents.fxml
 
@@ -94,22 +164,20 @@ public class MainController implements Initializable {
 			apTop.getChildren().add(apNavBar);
 			// seta apNavBar no lado direito da tela
 			AnchorPane.setRightAnchor(apNavBar, 0.0);
-			
 
-	        if (apDocument == null) {
-	            // Open the DocumentController
-	            FXMLLoader loader2 = new FXMLLoader(getClass().getResource("/fxml/Documents.fxml"));
-	            apDocument = loader2.load();
+			if (apDocument == null) {
+				// Open the DocumentController
+				FXMLLoader loader2 = new FXMLLoader(getClass().getResource("/fxml/Documents.fxml"));
+				apDocument = loader2.load();
 
-	            docController = loader2.getController();
-	            docController.setMainController(this);
+				docController = loader2.getController();
+				docController.setMainController(this);
 
-	            apManager.getChildren().add(apDocument);
-	        } else {
-	            // Close the DocumentController
-	            apManager.setVisible(!apDocument.isVisible());
-	        }
-			
+				apManager.getChildren().add(apDocument);
+			} else {
+				// Close the DocumentController
+				apManager.setVisible(!apDocument.isVisible());
+			}
 
 		} catch (IOException e) {
 			e.printStackTrace();
--- src/main/resources/html/map/script.js
+++ src/main/resources/html/map/script.js
@@ -1,6 +1,36 @@
- function initMap() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-            center: {lat: -15.7643276, lng: -47.83867},
-            zoom: 11
-        });
-    }
+// create a red polygon from an array of LatLng points
+/*var latlngs = [[37, -109.05],[41, -109.03],[41, -102.05],[37, -102.04]];
+
+var polygon = L.polygon(latlngs, {color: 'red'}).addTo(map);
+
+// zoom the map to the polygon
+map.fitBounds(polygon.getBounds());*/
+
+
+map.on('click', function(e) {
+    console.log(e.latlng);
+} );
+
+let json = {
+	"docId":1,
+	"docNumero":"12/2015",
+	"docProcesso":{"procId":2,"procNumero":"456/2015"},
+	"docSei":455,"docTipo":{"dtId":1,"dtDescricao":"Requerimento"},
+	"docEndereco":{
+		"endId":3,"endLogradouro":"Rua das Flores, Apartamento 5","endCidade":"br","endCEP":"22"
+	},
+	"intLatitude": -15.710520042184267,
+	"intLongitude": -47.76868726015565
+	}
+
+	
+	L.marker([json.intLatitude, json.intLongitude]).addTo(map);
+	
+	
+	function setZoom(){
+		alert('zoom');
+	}	
+	
+	app.setZoom();
+	
+	
