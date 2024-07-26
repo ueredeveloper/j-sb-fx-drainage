ce438d8a0dd84475ef73b7b8c3ffbf4c436bd9af
Fabrício José Barrozo <ueredeveloper@gmail.com>
2024-07-26 09:10:36 -0300

    fix: how to fetch address

--- src/main/java/controllers/views/AddressComboBoxController.java
+++ src/main/java/controllers/views/AddressComboBoxController.java
@@ -1,6 +1,7 @@
 package controllers.views;
 
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 import com.jfoenix.controls.JFXComboBox;
 
@@ -13,89 +14,92 @@ import services.EnderecoService;
 
 public class AddressComboBoxController {
 
-	
-	
-	String localUrl;
-
+	private String localUrl;
 	private JFXComboBox<Endereco> comboBox;
-	ObservableList<Endereco> obsAddress = FXCollections.observableArrayList();
+	private ObservableList<Endereco> obsList = FXCollections.observableArrayList();
+	private Set<Endereco> addressSet = new HashSet<>();
 
 	public AddressComboBoxController(String localUrl, JFXComboBox<Endereco> comboBox) {
-		super();
 		this.localUrl = localUrl;
 		this.comboBox = comboBox;
-
 		init();
 	}
 
-	// Método para inicializar o ComboBox
 	public void init() {
-		comboBox.setItems(obsAddress);
+		comboBox.setItems(obsList);
 		comboBox.setEditable(true);
 
 		utilities.FxUtilComboBoxSearchable.autoCompleteComboBoxPlus(comboBox, (typedText,
 				itemToCompare) -> itemToCompare.getEndLogradouro().toLowerCase().contains(typedText.toLowerCase()));
 
-		utilities.FxUtilComboBoxSearchable.getComboBoxValue(comboBox);
-
-		// Preenche com valores do servidor atualizando ao digitar
 		comboBox.getEditor().textProperty().addListener(new ChangeListener<String>() {
+			private String lastSearch = "";
+
 			@Override
 			public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
-				if (newValue != null) {
-					obsAddress.clear();
-					List<Endereco> list = fetchAddress(newValue);
+				if (newValue != null && !newValue.isEmpty() && newValue != "") {
+					obsList.clear();
 
-					if (list != null) {
-						boolean containsSearchTerm = list.stream()
-								.anyMatch(endereco -> endereco.getEndLogradouro().contains(newValue));
+					// Verifica se a nova busca é uma continuação da busca anterior, tanto
+					// adicionando como removendo caracteres
+					if (lastSearch.contains(newValue) || newValue.contains(lastSearch)) {
+						boolean containsSearchTerm = addressSet.stream().anyMatch(
+								endereco -> endereco.getEndLogradouro().toLowerCase().contains(newValue.toLowerCase()));
+
 						if (containsSearchTerm) {
-							obsAddress.addAll(list);
+							obsList.addAll(addressSet);
 						} else {
-							obsAddress.add(new Endereco(newValue));
-							obsAddress.addAll(list);
+							fetchAndUpdate(newValue);
 						}
+					} else {
+						// Nova busca completamente diferente, então limpamos o conjunto e fazemos uma
+						// nova busca
+						addressSet.clear();
+						fetchAndUpdate(newValue);
 					}
+
+					lastSearch = newValue;
 				}
 			}
 		});
 	}
 
-	public List<Endereco> fetchAddress(String keyword) {
+	private void fetchAndUpdate(String keyword) {
 		try {
 			EnderecoService service = new EnderecoService(localUrl);
-			List<Endereco> list = service.fetchAddressByKeyword(keyword);
-			return list;
+			Set<Endereco> fetchedAddresses = new HashSet<>(service.fetchAddressByKeyword(keyword));
+
+			if (!fetchedAddresses.isEmpty()) {
+				addressSet.addAll(fetchedAddresses);
+				obsList.addAll(addressSet);
+			} else {
+				// Se não houver resultados, adiciona o novo endereço diretamente
+				Endereco newAddress = new Endereco(keyword);
+				addressSet.add(newAddress);
+				obsList.add(newAddress);
+			}
 		} catch (Exception e) {
 			// Trate exceções adequadamente
+			e.printStackTrace();
 		}
-		return null;
 	}
 
 	public Endereco getSelectedObject() {
-		// Verifica se nulo e preenche objeto.
-		Endereco endereco = comboBox.selectionModelProperty().get().isEmpty() ? null
-				: new Endereco(obsAddress.get(0).getEndId(), obsAddress.get(0).getEndLogradouro());
-
-		return endereco;
-
+		// Verifica se nulo, se não nulo preenche objeto e retorna.
+		Endereco object = comboBox.selectionModelProperty().get().isEmpty() ? null
+				: new Endereco(obsList.get(0).getEndId(), obsList.get(0).getEndLogradouro());
+		return object;
 	}
 
 	public ObservableList<Endereco> getObservableList() {
-		return this.obsAddress;
+		return this.obsList;
 	}
 
 	public void fillAndSelectComboBox(Endereco object) {
 		ObservableList<Endereco> newObsList = FXCollections.observableArrayList();
+		newObsList.add(object);
 		comboBox.setItems(newObsList);
-
-		newObsList.add(0, object);
-
-		// Atualizando o ComboBox para refletir a mudança
-		comboBox.setItems(newObsList);
-
-		// Selecionando o novo item no ComboBox
-		comboBox.getSelectionModel().select(0);
+		comboBox.getSelectionModel().select(object);
 	}
 
 	public void clearComponent() {
